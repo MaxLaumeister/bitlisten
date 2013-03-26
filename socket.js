8184f4eb2385e82b79a4c55a1955cc42b843600c
Maximillian Laumeister <max.laumeister@gmail.com>
2013-03-25 22:23:47 -0700

    Mtgox socket.io js file now dynamically loaded.

--- socket.js
+++ socket.js
@@ -9,11 +9,10 @@ function TransactionSocket() {
 
 TransactionSocket.init = function() {
 	if ('WebSocket' in window) {
-		console.log("Websocket Supported");
 		var connection = new WebSocket('ws://ws.blockchain.info:8335/inv');
 
 		connection.onopen = function() {
-			console.log('Connection open!');
+			console.log('Blockchain.info: Connection open!');
 			var newTransactions = {
 				"op" : "unconfirmed_sub"
 			};
@@ -21,11 +20,11 @@ TransactionSocket.init = function() {
 		}
 
 		connection.onclose = function() {
-			console.log('Connection closed');
+			console.log('Blockchain.info: Connection closed');
 		}
 
 		connection.onerror = function(error) {
-			console.log('Error detected: ' + error);
+			console.log('Blockchain.info: Error detected: ' + error);
 		}
 
 		connection.onmessage = function(e) {
@@ -51,40 +50,46 @@ function TradeSocket() {
 }
 
 TradeSocket.init = function() {
-	var connection = io.connect('https://socketio.mtgox.com/mtgox');
+	// Load Mtgox socket.io library
+	$.getScript("https://socketio.mtgox.com/socket.io/socket.io.js", function() {
+		// Make connection to Mtgox
+		var connection = io.connect('https://socketio.mtgox.com/mtgox');
 
-	connection.on('connect', function() {
-		console.log('Connection open!');
+		connection.on('connect', function() {
+			console.log('Mtgox: Connection open!');
 
-		// Unsubscribe from depth and ticker
-		connection.emit('message', {
-			"op" : "unsubscribe",
-			"channel" : "24e67e0d-1cad-4cc0-9e7a-f8523ef460fe"
+			// Unsubscribe from depth and ticker
+			connection.emit('message', {
+				"op" : "unsubscribe",
+				"channel" : "24e67e0d-1cad-4cc0-9e7a-f8523ef460fe"
+			});
+			connection.emit('message', {
+				"op" : "unsubscribe",
+				"channel" : "d5f06780-30a8-4a48-a2f8-7ed181b4a13f"
+			});
 		});
-		connection.emit('message', {
-			"op" : "unsubscribe",
-			"channel" : "d5f06780-30a8-4a48-a2f8-7ed181b4a13f"
+
+		connection.on('disconnect', function() {
+			console.log('Mtgox: Connection closed');
 		});
+
+		connection.on('error', function() {
+			console.log('Mtgox: Error detected.');
+		});
+
+		connection.on('message', function(message) {
+			if (message.trade) {
+				console.log("Trade: " + message.trade.amount_int / satoshi + " BTC | " + (message.trade.price * message.trade.amount_int / satoshi) + " " + message.trade.price_currency);
+				// 0.57 BTC | 42.75 USD
+
+				var bitcoins = message.trade.amount_int / satoshi;
+				var currency = (message.trade.price * message.trade.amount_int / satoshi);
+				var currencyName = message.trade.price_currency;
+
+				new Transaction(bitcoins, currency, currencyName);
+			}
+		});
+
 	});
 
-	connection.on('disconnect', function() {
-		console.log('Connection closed');
-	});
-
-	connection.on('error', function() {
-		console.log('Error detected.');
-	});
-
-	connection.on('message', function(message) {
-		if (message.trade) {
-			console.log("Trade: " + message.trade.amount_int / satoshi + " BTC | " + (message.trade.price * message.trade.amount_int / satoshi) + " " + message.trade.price_currency); // 0.57 BTC | 42.75 USD
-			
-			var bitcoins = message.trade.amount_int / satoshi;
-			var currency = (message.trade.price * message.trade.amount_int / satoshi);
-			var currencyName = message.trade.price_currency;
-			
-			new Transaction(bitcoins, currency, currencyName);
-		}
-	});
 }
-
